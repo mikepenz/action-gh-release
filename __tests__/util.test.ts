7177a969a3a5a70944a2588374d2fa7c6a1eb360
Mike Penz <mikepenz@gmail.com>
2022-07-27 08:39:59 +0000

    - upgrade all dependencies to their latest stable release, fixing multiple security advisories - refactor the source and compile it via tsc - introduce license.txt generation - more cleanup and optimisations

--- __tests__/util.test.ts
+++ __tests__/util.test.ts
@@ -1,48 +1,42 @@
-import {
-  releaseBody,
-  isTag,
-  paths,
-  parseConfig,
-  parseInputFiles,
-  unmatchedPatterns,
-  uploadUrl
-} from "../src/util";
-import * as assert from "assert";
+import {releaseBody, isTag, paths, parseConfig, parseInputFiles, unmatchedPatterns, uploadUrl} from '../src/util'
+import * as assert from 'assert'
 
-describe("util", () => {
-  describe("uploadUrl", () => {
-    it("strips template", () => {
+describe('util', () => {
+  describe('uploadUrl', () => {
+    it('strips template', () => {
       assert.equal(
-        uploadUrl(
-          "https://uploads.github.com/repos/octocat/Hello-World/releases/1/assets{?name,label}"
-        ),
-        "https://uploads.github.com/repos/octocat/Hello-World/releases/1/assets"
-      );
-    });
-  });
-  describe("parseInputFiles", () => {
-    it("parses empty strings", () => {
-      assert.deepStrictEqual(parseInputFiles(""), []);
-    });
-    it("parses comma-delimited strings", () => {
-      assert.deepStrictEqual(parseInputFiles("foo,bar"), ["foo", "bar"]);
-    });
-    it("parses newline and comma-delimited (and then some)", () => {
-      assert.deepStrictEqual(
-        parseInputFiles("foo,bar\nbaz,boom,\n\ndoom,loom "),
-        ["foo", "bar", "baz", "boom", "doom", "loom"]
-      );
-    });
-  });
-  describe("releaseBody", () => {
-    it("uses input body", () => {
+        uploadUrl('https://uploads.github.com/repos/octocat/Hello-World/releases/1/assets{?name,label}'),
+        'https://uploads.github.com/repos/octocat/Hello-World/releases/1/assets'
+      )
+    })
+  })
+  describe('parseInputFiles', () => {
+    it('parses empty strings', () => {
+      assert.deepStrictEqual(parseInputFiles(''), [])
+    })
+    it('parses comma-delimited strings', () => {
+      assert.deepStrictEqual(parseInputFiles('foo,bar'), ['foo', 'bar'])
+    })
+    it('parses newline and comma-delimited (and then some)', () => {
+      assert.deepStrictEqual(parseInputFiles('foo,bar\nbaz,boom,\n\ndoom,loom '), [
+        'foo',
+        'bar',
+        'baz',
+        'boom',
+        'doom',
+        'loom'
+      ])
+    })
+  })
+  describe('releaseBody', () => {
+    it('uses input body', () => {
       assert.equal(
-        "foo",
+        'foo',
         releaseBody({
-          github_ref: "",
-          github_repository: "",
-          github_token: "",
-          input_body: "foo",
+          github_ref: '',
+          github_repository: '',
+          github_token: '',
+          input_body: 'foo',
           input_body_path: undefined,
           input_draft: false,
           input_prerelease: false,
@@ -53,17 +47,17 @@ describe("util", () => {
           input_discussion_category_name: undefined,
           input_generate_release_notes: false
         })
-      );
-    });
-    it("uses input body path", () => {
+      )
+    })
+    it('uses input body path', () => {
       assert.equal(
-        "bar",
+        'bar',
         releaseBody({
-          github_ref: "",
-          github_repository: "",
-          github_token: "",
+          github_ref: '',
+          github_repository: '',
+          github_token: '',
           input_body: undefined,
-          input_body_path: "__tests__/release.txt",
+          input_body_path: '__tests__/release.txt',
           input_draft: false,
           input_prerelease: false,
           input_files: [],
@@ -73,17 +67,17 @@ describe("util", () => {
           input_discussion_category_name: undefined,
           input_generate_release_notes: false
         })
-      );
-    });
-    it("defaults to body path when both body and body path are provided", () => {
+      )
+    })
+    it('defaults to body path when both body and body path are provided', () => {
       assert.equal(
-        "bar",
+        'bar',
         releaseBody({
-          github_ref: "",
-          github_repository: "",
-          github_token: "",
-          input_body: "foo",
-          input_body_path: "__tests__/release.txt",
+          github_ref: '',
+          github_repository: '',
+          github_token: '',
+          input_body: 'foo',
+          input_body_path: '__tests__/release.txt',
           input_draft: false,
           input_prerelease: false,
           input_files: [],
@@ -93,11 +87,11 @@ describe("util", () => {
           input_discussion_category_name: undefined,
           input_generate_release_notes: false
         })
-      );
-    });
-  });
-  describe("parseConfig", () => {
-    it("parses basic config", () => {
+      )
+    })
+  })
+  describe('parseConfig', () => {
+    it('parses basic config', () => {
       assert.deepStrictEqual(
         parseConfig({
           // note: inputs declared in actions.yml, even when declared not required,
@@ -106,13 +100,13 @@ describe("util", () => {
           // as an empty string !== undefined in terms of what we pass to the api
           // so we cover that in a test case here to ensure undefined values are actually
           // resolved as undefined and not empty strings
-          INPUT_TARGET_COMMITISH: "",
-          INPUT_DISCUSSION_CATEGORY_NAME: ""
+          INPUT_TARGET_COMMITISH: '',
+          INPUT_DISCUSSION_CATEGORY_NAME: ''
         }),
         {
-          github_ref: "",
-          github_repository: "",
-          github_token: "",
+          github_ref: '',
+          github_repository: '',
+          github_token: '',
           input_append_body: false,
           input_body: undefined,
           input_body_path: undefined,
@@ -126,18 +120,18 @@ describe("util", () => {
           input_discussion_category_name: undefined,
           input_generate_release_notes: false
         }
-      );
-    });
+      )
+    })
 
-    it("parses basic config with commitish", () => {
+    it('parses basic config with commitish', () => {
       assert.deepStrictEqual(
         parseConfig({
-          INPUT_TARGET_COMMITISH: "affa18ef97bc9db20076945705aba8c516139abd"
+          INPUT_TARGET_COMMITISH: 'affa18ef97bc9db20076945705aba8c516139abd'
         }),
         {
-          github_ref: "",
-          github_repository: "",
-          github_token: "",
+          github_ref: '',
+          github_repository: '',
+          github_token: '',
           input_append_body: false,
           input_body: undefined,
           input_body_path: undefined,
@@ -147,21 +141,21 @@ describe("util", () => {
           input_name: undefined,
           input_tag_name: undefined,
           input_fail_on_unmatched_files: false,
-          input_target_commitish: "affa18ef97bc9db20076945705aba8c516139abd",
+          input_target_commitish: 'affa18ef97bc9db20076945705aba8c516139abd',
           input_discussion_category_name: undefined,
           input_generate_release_notes: false
         }
-      );
-    });
-    it("supports discussion category names", () => {
+      )
+    })
+    it('supports discussion category names', () => {
       assert.deepStrictEqual(
         parseConfig({
-          INPUT_DISCUSSION_CATEGORY_NAME: "releases"
+          INPUT_DISCUSSION_CATEGORY_NAME: 'releases'
         }),
         {
-          github_ref: "",
-          github_repository: "",
-          github_token: "",
+          github_ref: '',
+          github_repository: '',
+          github_token: '',
           input_append_body: false,
           input_body: undefined,
           input_body_path: undefined,
@@ -172,21 +166,21 @@ describe("util", () => {
           input_tag_name: undefined,
           input_fail_on_unmatched_files: false,
           input_target_commitish: undefined,
-          input_discussion_category_name: "releases",
+          input_discussion_category_name: 'releases',
           input_generate_release_notes: false
         }
-      );
-    });
+      )
+    })
 
-    it("supports generating release notes", () => {
+    it('supports generating release notes', () => {
       assert.deepStrictEqual(
         parseConfig({
-          INPUT_GENERATE_RELEASE_NOTES: "true"
+          INPUT_GENERATE_RELEASE_NOTES: 'true'
         }),
         {
-          github_ref: "",
-          github_repository: "",
-          github_token: "",
+          github_ref: '',
+          github_repository: '',
+          github_token: '',
           input_append_body: false,
           input_body: undefined,
           input_body_path: undefined,
@@ -200,21 +194,21 @@ describe("util", () => {
           input_discussion_category_name: undefined,
           input_generate_release_notes: true
         }
-      );
-    });
+      )
+    })
 
-    it("prefers GITHUB_TOKEN over token input for backwards compatibility", () => {
+    it('prefers GITHUB_TOKEN over token input for backwards compatibility', () => {
       assert.deepStrictEqual(
         parseConfig({
-          INPUT_DRAFT: "false",
-          INPUT_PRERELEASE: "true",
-          GITHUB_TOKEN: "env-token",
-          INPUT_TOKEN: "input-token"
+          INPUT_DRAFT: 'false',
+          INPUT_PRERELEASE: 'true',
+          GITHUB_TOKEN: 'env-token',
+          INPUT_TOKEN: 'input-token'
         }),
         {
-          github_ref: "",
-          github_repository: "",
-          github_token: "env-token",
+          github_ref: '',
+          github_repository: '',
+          github_token: 'env-token',
           input_append_body: false,
           input_body: undefined,
           input_body_path: undefined,
@@ -228,19 +222,19 @@ describe("util", () => {
           input_discussion_category_name: undefined,
           input_generate_release_notes: false
         }
-      );
-    });
-    it("uses input token as the source of GITHUB_TOKEN by default", () => {
+      )
+    })
+    it('uses input token as the source of GITHUB_TOKEN by default', () => {
       assert.deepStrictEqual(
         parseConfig({
-          INPUT_DRAFT: "false",
-          INPUT_PRERELEASE: "true",
-          INPUT_TOKEN: "input-token"
+          INPUT_DRAFT: 'false',
+          INPUT_PRERELEASE: 'true',
+          INPUT_TOKEN: 'input-token'
         }),
         {
-          github_ref: "",
-          github_repository: "",
-          github_token: "input-token",
+          github_ref: '',
+          github_repository: '',
+          github_token: 'input-token',
           input_append_body: false,
           input_body: undefined,
           input_body_path: undefined,
@@ -254,18 +248,18 @@ describe("util", () => {
           input_discussion_category_name: undefined,
           input_generate_release_notes: false
         }
-      );
-    });
-    it("parses basic config with draft and prerelease", () => {
+      )
+    })
+    it('parses basic config with draft and prerelease', () => {
       assert.deepStrictEqual(
         parseConfig({
-          INPUT_DRAFT: "false",
-          INPUT_PRERELEASE: "true"
+          INPUT_DRAFT: 'false',
+          INPUT_PRERELEASE: 'true'
         }),
         {
-          github_ref: "",
-          github_repository: "",
-          github_token: "",
+          github_ref: '',
+          github_repository: '',
+          github_token: '',
           input_append_body: false,
           input_body: undefined,
           input_body_path: undefined,
@@ -279,17 +273,17 @@ describe("util", () => {
           input_discussion_category_name: undefined,
           input_generate_release_notes: false
         }
-      );
-    });
-    it("parses basic config with append_body", () => {
+      )
+    })
+    it('parses basic config with append_body', () => {
       assert.deepStrictEqual(
         parseConfig({
-          INPUT_APPEND_BODY: "true"
+          INPUT_APPEND_BODY: 'true'
         }),
         {
-          github_ref: "",
-          github_repository: "",
-          github_token: "",
+          github_ref: '',
+          github_repository: '',
+          github_token: '',
           input_append_body: true,
           input_body: undefined,
           input_body_path: undefined,
@@ -303,33 +297,29 @@ describe("util", () => {
           input_discussion_category_name: undefined,
           input_generate_release_notes: false
         }
-      );
-    });
-  });
-  describe("isTag", () => {
-    it("returns true for tags", async () => {
-      assert.equal(isTag("refs/tags/foo"), true);
-    });
-    it("returns false for other kinds of refs", async () => {
-      assert.equal(isTag("refs/heads/master"), false);
-    });
-  });
+      )
+    })
+  })
+  describe('isTag', () => {
+    it('returns true for tags', async () => {
+      assert.equal(isTag('refs/tags/foo'), true)
+    })
+    it('returns false for other kinds of refs', async () => {
+      assert.equal(isTag('refs/heads/master'), false)
+    })
+  })
 
-  describe("paths", () => {
-    it("resolves files given a set of paths", async () => {
-      assert.deepStrictEqual(
-        paths(["tests/data/**/*", "tests/data/does/not/exist/*"]),
-        ["tests/data/foo/bar.txt"]
-      );
-    });
-  });
+  describe('paths', () => {
+    it('resolves files given a set of paths', async () => {
+      assert.deepStrictEqual(paths(['tests/data/**/*', 'tests/data/does/not/exist/*']), ['tests/data/foo/bar.txt'])
+    })
+  })
 
-  describe("unmatchedPatterns", () => {
+  describe('unmatchedPatterns', () => {
     it("returns the patterns that don't match any files", async () => {
-      assert.deepStrictEqual(
-        unmatchedPatterns(["tests/data/**/*", "tests/data/does/not/exist/*"]),
-        ["tests/data/does/not/exist/*"]
-      );
-    });
-  });
-});
+      assert.deepStrictEqual(unmatchedPatterns(['tests/data/**/*', 'tests/data/does/not/exist/*']), [
+        'tests/data/does/not/exist/*'
+      ])
+    })
+  })
+})
